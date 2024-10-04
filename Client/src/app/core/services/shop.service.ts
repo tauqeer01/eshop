8834bcb4ee00af6b3b6995fcb75837b4b91741af
tauqeer01 <aerotech.tk@gmail.com>
2024-10-04 22:41:58 +0530

    updated product details

--- Client/src/app/core/services/shop.service.ts
+++ Client/src/app/core/services/shop.service.ts
@@ -32,6 +32,10 @@ export class ShopService {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'product',{params});
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'product/'+id);
+  }
+
   getTypes() {
     if(this.types.length>0) return;
     return this.http.get<string[]>(this.baseUrl + 'product/types').subscribe({
